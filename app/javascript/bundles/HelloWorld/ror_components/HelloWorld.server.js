f4eabe97a9c36b6dc9ff0f52df836242182735b0
Abanoub Ghadban <ghdbana@gmail.com>
2025-09-11 20:16:32 +0300

    enable auto_bundle

--- app/javascript/bundles/HelloWorld/ror_components/HelloWorld.server.js
+++ app/javascript/bundles/HelloWorld/ror_components/HelloWorld.server.js
@@ -1,5 +1,3 @@
-import HelloWorld from './HelloWorld';
 // This could be specialized for server rendering
 // For example, if using React Router, we'd have the SSR setup here.
-
-export default HelloWorld;
+export { default } from '../components/HelloWorld';
